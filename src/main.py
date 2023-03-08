702acfd3da53a8be9691a3a6ba4405d777468c3e
Radu Guran <radu.guran@softvision.com>
2023-03-08 10:08:43 +0200

    Added code explanation;

--- src/main.py
+++ src/main.py
@@ -1,28 +1,35 @@
 import argparse
 import csv
-import response
 import urllib3 as urllib3
 from locate_coffee_shop import locate_closest_coffee_shops
 
 
 if __name__ == '__main__':
+
+    #parser initialization in order to add custom arguments
+    #parser creation
     parser = argparse.ArgumentParser()
-    parser.add_argument("X", type=float, help="Provide the user x coordinate")
-    parser.add_argument("Y", type=float, help="Provide the user y coordinate")
-    parser.add_argument("shops_url", type=str, help="Provide the shop data url")
+    #added arguments (command line field name, variable type )
+    parser.add_argument("user_x", type=float)
+    parser.add_argument("user_y", type=float)
+    parser.add_argument("shops_url", type=str)
+    #parse the argument
     args = parser.parse_args()
-
-    user_loc = [args.x, args.y]
-
+    #save input user coordinates in user_loc variable
+    user_loc = [args.user_x, args.user_y]
+    #use urllib3 in order to get the http request of the provided URL
     http = urllib3.PoolManager()
     response = http.request('GET', args.shops_url)
-    if response.status != 200:  # checking for the OK status
+    #check for the HTTP 200 OK status that indicates the request succeeded
+    if response.status != 200:
         print(f"The URL you provided generated a code {response.status}: {response.reason}")
         exit()
-
+    #read the csv file found at the particular location
     csv_file = csv.reader(response.data.decode('utf-8').split("\n"))
+    #return the content of the csv file in a list
     coffee_shops = list(csv_file)
+    #locate the three closest coffee shops from the user location
     coffee_shops = locate_closest_coffee_shops(user_loc, coffee_shops)
-
+    #print the three closest coffee shops from the user location
     for shop in coffee_shops:
         print(f"{shop.name},{shop.distance}")
--- tests/unit_test_coffee_shop.py
+++ tests/unit_test_coffee_shop.py
@@ -19,18 +19,20 @@ class TestLocateCoffeeShop(unittest.TestCase):
 
     def test_locate_coffee_shop_fail(self):
         user_location = [0, 0]
-        coffee_shops = [['Loc 1', '1', '1'],
-                        ['Loc 2', '2', '2'],
-                        ['Loc 3', '3', 'cartof'],
-                        ['Loc 4', '4'],
+        coffee_shops = [['Loc 1', 1, 1],
+                        ['Loc 2', 2, 2],
+                        ['Loc 3', '3', 'string'],
+                        ['Loc 4', 4],
                         [],
-                        ['Loc 5', '5', '5']]
+                        ['Loc 5', 5, 5]]
         expected_response = [ProximateCoffeeShop(name='Loc 1', distance=1.4142),
                              ProximateCoffeeShop(name='Loc 2', distance=2.8284),
                              ProximateCoffeeShop(name='Loc 5', distance=7.0711)]
-        expected_issues = ["Issue at row number 3: could not convert string to float: 'cartof' ",
-                           'Issue at row number 4: not enough values to unpack (expected 3, got 2) ',
-                           'Issue at row number 5: not enough values to unpack (expected 3, got 0) ']
+        '''Expected issues: - Could not convert string to float: 'string' ,
+                            - Not enough values to unpack (expected 3, got 2) ,
+                            - Not enough values to unpack (expected 3, got 0) ,
+                            - Input file does not exist or provided url is not correct ,
+        '''
         self.assertEqual(locate_closest_coffee_shops(user_location[0], user_location[1], coffee_shops), expected_response)
 
 if __name__ == '__main__':
