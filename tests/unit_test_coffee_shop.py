503346f86220ea3aba3bea0bc9582b47d994fb8d
Radu Guran <radu.guran@softvision.com>
2023-03-08 11:35:05 +0200

    Updated unit tests;

--- tests/unit_test_coffee_shop.py
+++ tests/unit_test_coffee_shop.py
@@ -4,6 +4,16 @@ from src.locate_coffee_shop import ProximateCoffeeShop, locate_closest_coffee_sh
 
 class TestLocateCoffeeShop(unittest.TestCase):
 
+    # test the existence of the file path
+    def test_locate_coffee_shop_no_file(self):
+        file_path = 'fake_file.csv'
+        user_x = 47.5809
+        user_y = -122.3160
+        try:
+            locate_closest_coffee_shops(user_x, user_y, file_path)
+        except Exception as ex:
+            assert "Input file does not exist." in str(ex)
+
     def test_locate_coffee_shop(self):
         user_location = [0, 0]
         coffee_shops = [['Loc 1', 1, 1],
@@ -15,7 +25,8 @@ class TestLocateCoffeeShop(unittest.TestCase):
         expected_response = [ProximateCoffeeShop(name='Loc 1', distance=1.4142),
                              ProximateCoffeeShop(name='Loc 2', distance=2.8284),
                              ProximateCoffeeShop(name='Loc 3', distance=4.2426)]
-        self.assertEqual(locate_closest_coffee_shops(user_location[0], user_location[1], coffee_shops), expected_response)
+        self.assertEqual(locate_closest_coffee_shops(user_location[0], user_location[1], coffee_shops),
+                         expected_response)
 
     def test_locate_coffee_shop_fail(self):
         user_location = [0, 0]
@@ -31,9 +42,10 @@ class TestLocateCoffeeShop(unittest.TestCase):
         '''Expected issues: - Could not convert string to float: 'string' ,
                             - Not enough values to unpack (expected 3, got 2) ,
                             - Not enough values to unpack (expected 3, got 0) ,
-                            - Input file does not exist or provided url is not correct ,
         '''
-        self.assertEqual(locate_closest_coffee_shops(user_location[0], user_location[1], coffee_shops), expected_response)
+        self.assertEqual(locate_closest_coffee_shops(user_location[0], user_location[1], coffee_shops),
+                         expected_response)
+
 
 if __name__ == '__main__':
     unittest.main()
