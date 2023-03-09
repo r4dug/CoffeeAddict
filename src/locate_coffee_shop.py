de392a3f43fc20d6a1fa1d75d080b614984efb01
Radu Guran <radu.guran@softvision.com>
2023-03-09 12:16:17 +0200

    1)locate_closest_coffee_shops function in locate_coffee_shop.py has been simplified/corrected; 2) main.py file has beed updated and now 'requests' library is being used instead of urllib3; 3) venv was added in .gitignore;

--- src/locate_coffee_shop.py
+++ src/locate_coffee_shop.py
@@ -1,35 +1,26 @@
-import math
+from scipy.spatial import distance
 from collections import namedtuple
 
 ProximateCoffeeShop = namedtuple("CoffeeShop", "name distance")
 
 """
-Function that calculates the distance between two points, which is an application of the Pythagorean theorem.
-:param x1: represents X coordinate of the 1st point
-:param x2: represents X coordinate of the 2nd point
-:param y1: represents Y coordinate of the 1st point
-:param y2: represents Y coordinate of the 2nd point
-:return: the result of the distance between two points
-"""
-def get_distance_between_two_points(x1, y1, x2, y2):
-    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-
-"""
 Function that locates the closest coffee shop locations from the user's location.
-:param user_x: represents user's X coordinate
-:param user_y: represents user's Y coordinate
+:param user_location: is a tuple of 2 floats representing the x and y coordinates
 :param coffee_shops: represents the coffee shop's name along it's X and Y coordinates in a tuple
 :return: the closest 3 coffee shops
 """
-def locate_closest_coffee_shops(user_x, user_y, coffee_shops):
+def locate_closest_coffee_shops(user_location, coffee_shops):
     shops = []
-    for shop in coffee_shops:
+    for row, shop_data in enumerate(coffee_shops, 1):
         try:
+            shop_name, user_x, user_y = shop_data
+            x = float(user_x)
+            y = float(user_y)
             # find distance between two points
-            shop_distance = get_distance_between_two_points(user_x, user_y, shop[1], shop[2])
-            shops.append(ProximateCoffeeShop(shop[0], shop_distance))
+            shop_distance = round(distance.euclidean(user_location, [x, y]), 4)
+            shops.append(ProximateCoffeeShop(shop_name, shop_distance))
         except ValueError as err:
-            print(f"Execution stopped. The following error occurred: {err}")
+            print(f"Execution stopped. The following error occurred: {err} at row {row}")
     # sort the coffee shops by distance
-    shops.sort(key=lambda asc: asc.distance)
-    return shops[:3]
+    sorted_shops = sorted(shops, key=lambda asc: asc.distance)
+    return sorted_shops[:3]
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import argparse
 import csv
-import urllib3 as urllib3
+import requests
+from contextlib import closing
+from codecs import iterdecode
 from locate_coffee_shop import locate_closest_coffee_shops
 
 
@@ -17,19 +19,14 @@ if __name__ == '__main__':
     args = parser.parse_args()
     #save input user coordinates in user_loc variable
     user_loc = [args.user_x, args.user_y]
-    #use urllib3 in order to get the http request of the provided URL
-    http = urllib3.PoolManager()
-    response = http.request('GET', args.shops_url)
-    #check for the HTTP 200 OK status that indicates the request succeeded
-    if response.status != 200:
-        print(f"The URL you provided generated a code {response.status}: {response.reason}")
-        exit()
-    #read the csv file found at the particular location
-    csv_file = csv.reader(response.data.decode('utf-8').split("\n"))
-    #return the content of the csv file in a list
-    coffee_shops = list(csv_file)
-    #locate the three closest coffee shops from the user location
-    coffee_shops = locate_closest_coffee_shops(user_loc, coffee_shops)
+    #use requests in order to get the http request of the provided URL
+    with requests.get(args.shops_url, stream=True) as r:
+        lines = (line.decode('utf-8') for line in r.iter_lines())
+        csv_coffee_shops = []
+        for row in csv.reader(lines):
+            csv_coffee_shops.append(row)
+        # locate the three closest coffee shops from the user location
+        coffee_shops = locate_closest_coffee_shops(user_loc,csv_coffee_shops)
     #print the three closest coffee shops from the user location
     for shop in coffee_shops:
         print(f"{shop.name},{shop.distance}")
